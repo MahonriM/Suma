b7cd2af4144badb497499ac447b2897a13a6e950
MahonriM <cripto3000z@gmail.com>
2018-10-26 14:21:37 -0500

    Suma
    Suma

---
+++ Suma/src/Suma.java
@@ -0,0 +1,12 @@
+
+public class Suma {
+
+    public static void main(String[] args) {
+	
+	 double AVOGADRO = 6.023e23;
+	 
+	 System.out.println( "double" + AVOGADRO ); 
+	 //este program sirve para imprimir
+    }
+
+}
